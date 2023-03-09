8124b58c3ee8efd79b4868678b96a1344674d4f2
Colm O'Shea <colm.oshea@adaptivemobile.com>
2023-03-09 11:39:00 +0000

    add python template

---
+++ python/script_template.py
@@ -0,0 +1,47 @@
+from optparse import OptionParser
+from os import listdir
+from os.path import isfile, join
+import re
+import sys
+
+
+
+"""
+A python script template for to demonstrate reading arguments and processing a directory.
+"""
+
+def parse_options():
+    parser = OptionParser()
+    parser.add_option("--source-dir",  dest="source_dir", default = None, type = "string",
+                      help="Source directory")
+    opt, args = parser.parse_args()
+    if not opt.source_dir:
+        sys.exit('Error: You need to supply a directory name using the --source-dir argument.')
+    return opt
+
+def get_all_files_from_directory(directory):
+    files = []
+    onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
+    for f in onlyfiles:
+        full_path = join(directory, f)
+        files.append(full_path)
+    return files
+
+def process_a_file(filename):
+    content = open(filenme).read()
+
+def extract_html_paragraphs(content):
+    '''Extract all html paragraphs from a string'''
+    para_regex = re.compile('''<p>.*?</p>''', re.DOTALL)
+    para_matches = para_regex.findall(content)
+    return para_matches
+
+
+def test_extract_html_paragraphs():
+    assert extract_html_paragraphs('start<p>value 1</p>')[0] == '<p>value 1</p>'
+    assert extract_html_paragraphs('<p>value 1</p><p>value 2</p>')[1] == '<p>value 2</p>'
+    assert extract_html_paragraphs('<p>value\n 1</p>')[0] == '<p>value\n 1</p>'
+
+if __name__ == '__main__':
+    opt = parse_options()
+    f = get_all_files_from_directory(opt.source_dir)
